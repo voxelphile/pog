21429926842ef543767d91d04ede1053b12e3483
brynnbrancamp <brynnbrancamp@gmail.com>
2023-02-16 14:30:14 -0600

    Triangle with extra steps.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-#![feature(box_syntax)]
-
-use std::{ future::Future, task::{ Context, Poll }, pin::Pin };
+use std::{
+    mem,
+    num::NonZeroU64,
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::Window,
     window::WindowBuilder,
 };
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,11 +19,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let state = State::new().await;
 
-    State::run(state).await;
-    
+    State::run(state);
+
     Ok(())
 }
-
+/*
 type Identifier = u32;
 
 struct Chunk {}
@@ -49,14 +53,9 @@ enum EntityState {
     Spawn,
     Active(Box<dyn Entity>),
     Despawn,
-}
+}*/
 
 struct State {
-    inner: Arc<Mutex<Inner>>,
-}
-
-struct Inner {
-    entities: Vec<EntityState>,
     event_loop: EventLoop<()>,
     graphics: Graphics,
 }
@@ -66,18 +65,17 @@ impl State {
         let event_loop = EventLoop::new();
         Self {
             graphics: Graphics::new(&event_loop).await,
-            entities: vec![],
+            //entities: vec![],
             event_loop,
         }
     }
 
-    async fn run(mut self) -> Completion {
-        let Self {
-            event_loop
-            , ..
-        } = self;
+    fn run(mut self) {
+        let Self { event_loop, .. } = self;
 
-        loop {
+        let mut cursor_captured = false;
+        let mut perframe_data = PerframeData::default();
+
         event_loop.run(move |event, _, control_flow| match event {
             Event::WindowEvent {
                 ref event,
@@ -92,8 +90,98 @@ impl State {
                 }
                 _ => {}
             },
+             Event::WindowEvent {
+                event: WindowEvent::CursorMoved { position, .. },
+                window_id,
+            } => {
+                if cursor_captured {
+                    let winit::dpi::PhysicalPosition { x, y } = position;
+
+                    let winit::dpi::PhysicalSize { width, height } = self.graphics.window.inner_size();
+
+                    let x_diff = x - width as f64 / 2.0;
+                    let y_diff = y - height as f64 / 2.0;
+
+                    self.graphics.window.set_cursor_position(winit::dpi::PhysicalPosition::new(
+                        width as i32 / 2,
+                        height as i32 / 2,
+                    ));
+
+                    perframe_data.look_x += x_diff as f32;
+                    perframe_data.look_y += y_diff as f32;
+                }
+            }
+            Event::WindowEvent {
+                event: WindowEvent::MouseInput { button, .. },
+                window_id,
+            } => {
+                use winit::event::MouseButton::*;
+                
+                match button {
+                    Left => {
+                        cursor_captured = true;
+                        self.graphics.window.set_cursor_icon(winit::window::CursorIcon::Crosshair);
+                        self.graphics.window
+                            .set_cursor_grab(winit::window::CursorGrabMode::Confined)
+                            .expect("could not grab mouse cursor");
+                        perframe_data.action1 = true as _;     
+                    }
+                    Right => {
+                        perframe_data.action2 = true as _;
+                    }
+                    _ => {}
+                }
+            }
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput { input, .. },
+                window_id,
+            } => {
+                let Some(key_code) = input.virtual_keycode else {
+                    return;
+                };
+
+                use winit::event::VirtualKeyCode::*;
+
+                match key_code {
+                    W => {
+                        perframe_data.forward =
+                            (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    A => {
+                        perframe_data.left =
+                            (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    S => {
+                        perframe_data.backward =
+                            (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    D => {
+                        perframe_data.right =
+                            (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    Space => {
+                        perframe_data.up = (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    LShift => {
+                        perframe_data.down =
+                            (input.state == winit::event::ElementState::Pressed) as _
+                    }
+                    Escape => {
+                        cursor_captured = false;
+                        self.graphics.window.set_cursor_icon(winit::window::CursorIcon::Default);
+                        self.graphics.window
+                            .set_cursor_grab(winit::window::CursorGrabMode::None)
+                            .expect("could not grab mouse cursor");
+                    }
+                    _ => {}
+                };
+            }
             Event::RedrawRequested(window_id) if window_id == self.graphics.window.id() => {
-                match self.graphics.render() {
+                match self.graphics.render(perframe_data) {
+                    Ok(_) => {
+                        perframe_data.look_x = 0.0;
+                        perframe_data.look_y = 0.0;
+                    },
                     Err(wgpu::SurfaceError::Outdated) => self.graphics.resize(),
                     Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
                     _ => {}
@@ -104,11 +192,30 @@ impl State {
             }
             _ => {}
         });
-        }
-
-        Poll::Pending
     }
 }
+
+#[repr(C)]
+#[derive(Default, Copy, Clone)]
+struct PerframeData {
+    fov: f32,
+    near: f32,
+    far: f32,
+    aspect_ratio: f32,
+    up: u32,
+    down: u32,
+    left: u32,
+    right: u32,
+    forward: u32,
+    backward: u32,
+    action1: u32,
+    action2: u32,
+    look_x: f32,
+    look_y: f32,
+}
+
+unsafe impl bytemuck::Zeroable for PerframeData {}
+unsafe impl bytemuck::Pod for PerframeData {}
 
 struct Graphics {
     surface: wgpu::Surface,
@@ -118,11 +225,20 @@ struct Graphics {
     window_size: winit::dpi::PhysicalSize<u32>,
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
+    setup_pipeline: wgpu::ComputePipeline,
+    perframe_pipeline: wgpu::ComputePipeline,
+    build_pipeline: wgpu::ComputePipeline,
+    data_buffer: wgpu::Buffer,
+    indirect_buffer: wgpu::Buffer,
+    perframe_buffer: wgpu::Buffer,
+    region_texture: wgpu::Texture,
+    region_texture_view: wgpu::TextureView,
+    bind_group: wgpu::BindGroup,
 }
 
 impl Graphics {
     async fn new(event_loop: &EventLoop<()>) -> Self {
-        let window = WindowBuilder::new().build(&event_loop).unwrap(); 
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
 
         let window_size = window.inner_size();
 
@@ -151,7 +267,7 @@ impl Graphics {
         let (device, queue) = adapter
             .request_device(
                 &wgpu::DeviceDescriptor {
-                    features: wgpu::Features::empty(),
+                    features: wgpu::Features::VERTEX_WRITABLE_STORAGE,
                     limits: if cfg!(target_arch = "wasm32") {
                         wgpu::Limits::downlevel_webgl2_defaults()
                     } else {
@@ -182,19 +298,162 @@ impl Graphics {
             view_formats: vec![],
         };
         surface.configure(&device, &config);
+        
+        let indirect_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Indirect Buffer"),
+            usage: wgpu::BufferUsages::INDIRECT | wgpu::BufferUsages::COPY_DST,
+            size: 1_000_000,
+            mapped_at_creation: false,
+        });
 
-        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        let data_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Data Buffer"),
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+            size: 1_000_000,
+            mapped_at_creation: false,
+        });
+        
+        let perframe_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Data Buffer"),
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+            size: 1_000_000,
+            mapped_at_creation: false,
+        });
 
-        let render_pipeline_layout =
+
+        let region_texture = device.create_texture(
+    &wgpu::TextureDescriptor {
+        // All textures are stored as 3D, we represent our 2D texture
+        // by setting depth to 1.
+        size: wgpu::Extent3d {
+            width: 256,
+            height: 256,
+            depth_or_array_layers: 256,
+        },
+        mip_level_count: 1, // We'll talk about this a little later
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D3,
+        // Most images are stored using sRGB so we need to reflect that here.
+        format: wgpu::TextureFormat::R32Uint,
+        // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
+        // COPY_DST means that we want to copy data to this texture
+        usage: wgpu::TextureUsages::STORAGE_BINDING | wgpu::TextureUsages::TEXTURE_BINDING,
+        label: Some("Region texture"),
+        // This is the same as with the SurfaceConfig. It
+        // specifies what texture formats can be used to
+        // create TextureViews for this texture. The base
+        // texture format (Rgba8UnormSrgb in this case) is
+        // always supported. Note that using a different
+        // texture format is not supported on the WebGL2
+        // backend.
+        view_formats: &[],
+    }
+    );
+
+    let region_texture_view = region_texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX 
+                    | wgpu::ShaderStages::FRAGMENT
+                    | wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Texture {
+                    sample_type: wgpu::TextureSampleType::Uint,
+                    view_dimension: wgpu::TextureViewDimension::D3,
+                    multisampled: false,
+                },
+                count: None,
+            },
+                wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::VERTEX 
+                    | wgpu::ShaderStages::FRAGMENT
+                    | wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: Some(NonZeroU64::new(1_000_000).unwrap()),
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 2,
+                visibility: wgpu::ShaderStages::VERTEX 
+                    | wgpu::ShaderStages::FRAGMENT
+                    | wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: Some(NonZeroU64::new(1_000_000).unwrap()),
+                },
+                count: None,
+            },
+            ],
+            label: Some("bind_group_layout"),
+        });
+
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&region_texture_view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: &data_buffer,
+                        offset: 0,
+                        size: Some(NonZeroU64::new(1_000_000).unwrap()),
+                    }),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: &perframe_buffer,
+                        offset: 0,
+                        size: Some(NonZeroU64::new(1_000_000).unwrap()),
+                    }),
+                },
+            ],
+            label: Some("bind_group"),
+        });
+
+        let pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&bind_group_layout],
                 push_constant_ranges: &[],
             });
 
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        
+        let perframe_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &shader,
+            entry_point: "cs_perframe",
+        });
+        
+        let build_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &shader,
+            entry_point: "cs_build",
+        });
+        
+        let setup_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &shader,
+            entry_point: "cs_setup",
+        });
+
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
+            layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main", // 1.
@@ -240,6 +499,15 @@ impl Graphics {
             config,
             window_size,
             render_pipeline,
+            perframe_pipeline,
+            setup_pipeline,
+            build_pipeline,
+            perframe_buffer,
+            data_buffer,
+            indirect_buffer,
+            region_texture,
+            region_texture_view,
+            bind_group,
         }
     }
 
@@ -251,18 +519,60 @@ impl Graphics {
         }
     }
 
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+    fn render(&mut self, perframe_data: PerframeData) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
 
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
 
+        self.queue.write_buffer(&self.perframe_buffer, 0, bytemuck::cast_slice(&[perframe_data]));
+
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                 label: Some("Render Encoder"),
             });
+
+        {
+            let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: Some("Compute Pass") });
+
+            compute_pass.set_bind_group(0, &self.bind_group, &[]);
+
+            compute_pass.set_pipeline(&self.perframe_pipeline);
+            compute_pass.dispatch_workgroups(1,1,1);
+
+        }
+
+        encoder.copy_buffer_to_buffer(
+            &self.data_buffer, 
+            0, 
+            &self.indirect_buffer, 
+            mem::size_of::<wgpu::util::DrawIndirect>() as u64,
+            2 * mem::size_of::<wgpu::util::DispatchIndirect>() as u64
+        );
+
+        {
+            let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: Some("Compute Pass") });
+
+            compute_pass.set_bind_group(0, &self.bind_group, &[]);
+
+            let mut offset = mem::size_of::<wgpu::util::DrawIndirect>() as u64;
+            compute_pass.set_pipeline(&self.build_pipeline);
+            compute_pass.dispatch_workgroups_indirect(&self.indirect_buffer, offset); 
+            
+            offset += mem::size_of::<wgpu::util::DispatchIndirect>() as u64;
+            compute_pass.set_pipeline(&self.setup_pipeline);
+            compute_pass.dispatch_workgroups_indirect(&self.indirect_buffer, offset);
+        }
+
+        encoder.copy_buffer_to_buffer(
+            &self.data_buffer, 
+            0, 
+            &self.indirect_buffer, 
+            0,
+            mem::size_of::<wgpu::util::DrawIndirect>() as u64,
+        );
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
@@ -282,9 +592,11 @@ impl Graphics {
                 })],
                 depth_stencil_attachment: None,
             });
+            
+            render_pass.set_bind_group(0, &self.bind_group, &[]);
 
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw_indirect(&self.indirect_buffer, 0);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
